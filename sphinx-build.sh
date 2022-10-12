baa7d0d51e351cdca5b12c398657b600a63bf328
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-10-12 08:49:14 -0400

    adding files

--- sphinx-build.sh
+++ sphinx-build.sh
@@ -4,10 +4,9 @@ set -x
 
 COMMAND=(/bin/bash -c "pip install --upgrade pip setuptools wheel ; pip install -r requirements.txt -U ; make -C docs clean html")
 
-. ./containthedocs-image
 
 exec docker run --rm -t \
   -v "$PWD":"$PWD" --workdir "$PWD" \
   ${DOCKER_RUN_ARGS} \
   -e "LOCAL_USER_ID=$(id -u)" \
-  ${DOC_IMG} "${COMMAND[@]}"
+  sphinxdoc/sphinx:latest "${COMMAND[@]}"
