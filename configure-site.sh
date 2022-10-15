b05cda343882145be6999c14331f341a7c387f2a
robinmordasiewicz <robin@mordasiewicz.com>
2022-10-15 15:24:46 -0400

    updating template

--- configure-site.sh
+++ configure-site.sh
@@ -38,6 +38,7 @@ if [ ! "${nodename}" ]; then exit; fi
 echo "# Create manifests"
 cd manifests/
 
+echo "# Create K8s clsuter"
 jq -r ".metadata.name = \"${sitename}\" | .spec.cluster_wide_app_list.cluster_wide_apps[].argo_cd.local_domain.password.blindfold_secret_info.location = \"<removed>\" " k8s_cluster.json | sponge k8s_cluster.json
 git add k8s_cluster.json && git commit --quiet -m "creating deployment manifests"
 
@@ -45,11 +46,14 @@ echo "# Create an appstack site"
 jq -r ".metadata.name = \"${sitename}\" | .spec.k8s_cluster.name = \"${sitename}\" | .spec.master_nodes[] = \"${nodename}\" | .spec.address = \"${address}\" | .spec.coordinates.latitude = \"${latitude}\" | .spec.coordinates.longitude = \"${longitude}\" " appstack_site.json | sponge appstack_site.json
 git add appstack_site.json && git commit --quiet -m "creating deployment manifests"
 
-echo "# Create a site token for registration"
+echo "# Create a site registration approval"
 jq -r ".metadata.name = \"${sitename}-token\" " token.json | sponge token.json
 git add token.json && git commit --quiet -m "creating deployment manifests"
 
-echo "# Register the CE"
+echo "# Create CE registraion"
 jq -r ".cluster_name = \"${sitename}\" | .hostname = \"${nodename}\" | .latitude = \"${latitude}\" | .longitude = \"${longitude}\" " ce-register.json | sponge ce-register.json
 git add ce-register.json && git commit --quiet -m "creating deployment manifests"
+
+echo "# Update documentation"
+
 
