14fff01b27aeb866131fa244084291410892d270
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-10-16 01:45:53 -0400

    adding files

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,25 +1,63 @@
 #!/bin/bash
 #
 
-echo "# Set up tenant name and check for credentials"
-read -p "Tenant Name: [f5-amer-ent] " tenantname
-tenantname="${tenantname:=f5-amer-ent}"
+git fetch
 
-echo "checking for p12 cert"
-openssl pkcs12 -in ~/${tenantname}.console.ves.volterra.io.api-creds.p12 -nodes -nokeys -out ~/vescred.cert
-openssl pkcs12 -in ~/${tenantname}.console.ves.volterra.io.api-creds.p12 -nodes -nocerts -out ~/vesprivate.key
+function is_in_local() {
+    local branch=${1}
+    local existed_in_local=$(git branch --list ${branch})
 
-cat <<EOF > ~/.vesconfig
-server-urls: https://${tenantname}.console.ves.volterra.io/api
-key: $HOME/vesprivate.key
-cert: $HOME/vescred.cert
-EOF
+    if [[ -z ${existed_in_local} ]]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
 
-read -p "Site Name: " sitename
-if [ ! "${sitename}" ]; then exit; fi
+function is_in_remote() {
+    local branch=${1}
+    local existed_in_remote=$(git ls-remote --heads origin ${branch})
 
-git checkout -b ${sitename}
+    if [[ -z ${existed_in_remote} ]]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
 
+currentbranch=`git branch --show-current`
+echo "current branch = $currentbranch"
+
+if [[ ${currentbranch} == "main" ]]; then
+  # in main configure site
+  read -p "Site Name: " sitename
+  if [ ! "${sitename}" ]; then echo "Error: no sitename provided" ;exit; fi
+  if [[ `is_in_local ${sitename}` == 1 ]]; then
+    git switch ${sitename} && jq -r ".sitename = \"${sitename}\" " manifests/site.json | sponge manifests/site.json && git add manifests/site.json && git commit --quiet -m "configuring site manifest"
+    git merge main -m "Merge master"
+  elif [[ `is_in_remote ${sitename}` == 1 ]]; then
+    git switch -c ${sitename} origin/${sitename} && jq -r ".sitename = \"${sitename}\" " manifests/site.json | sponge manifests/site.json && git add manifests/site.json && git commit --quiet -m "configuring site manifest"
+    git merge main -m "Merge master"
+  else
+    git checkout -b ${sitename} && jq -r ".sitename = \"${sitename}\" " manifests/site.json | sponge manifests/site.json && git add manifests/site.json && git commit --quiet -m "configuring site manifest"
+  fi
+  currentbranch=`git branch --show-current`
+else
+  # reconfiguring a site
+  currentbranch=`git branch --show-current`
+  read -p "Site Name: [${currentbranch}] " sitename
+  sitename="${sitename:=${currentbranch}}"
+  if [[ ${currentbranch} != ${sitename} ]]; then
+    if [[ `is_in_local ${sitename}` == 1 ]]; then
+      git switch ${sitename} && jq -r ".sitename = \"${sitename}\" " manifests/site.json | sponge manifests/site.json && git add manifests/site.json && git commit --quiet -m "configuring site manifest"
+    else
+      git checkout -b ${sitename} && jq -r ".sitename = \"${sitename}\" " manifests/site.json | sponge manifests/site.json && git add manifests/site.json && git commit --quiet -m "configuring site manifest"
+    fi
+    currentbranch=`git branch --show-current`
+  fi
+fi
+
+sitename=$currentbranch
 
 timeout () {
     tput sc
@@ -32,12 +70,31 @@ timeout () {
     tput rc; tput ed;
 }
 
+cd manifests/
+
+
+echo "# Set up tenant name and check for credentials"
+currenttenantname=`jq -r ".tenantname" site.json`
+read -p "Tenant Name:: [${currenttenantname}] " tenantname
+address="${address:=${currenttenantname}}"
+
+echo "checking for p12 cert"
+openssl pkcs12 -in ~/${tenantname}.console.ves.volterra.io.api-creds.p12 -nodes -nokeys -out ~/vescred.cert
+openssl pkcs12 -in ~/${tenantname}.console.ves.volterra.io.api-creds.p12 -nodes -nocerts -out ~/vesprivate.key
+
+cat <<EOF > ~/.vesconfig
+server-urls: https://${tenantname}.console.ves.volterra.io/api
+key: $HOME/vesprivate.key
+cert: $HOME/vescred.cert
+EOF
 
 read -s -p "CE node new password: " newpassword
 if [ ! "${newpassword}" ]; then exit; fi
 echo "*********************"
 
 echo "# Change the CE password"
+cenodeaddress=`jq -r ".cenodeaddress" site.json`
+cenodeport=`jq -r ".cenodeport" site.json`
 curl -sS -k -v "https://${cenodeaddress}:${cenodeport}/api/ves.io.vpm/introspect/write/ves.io.vpm.node/change-password" \
   -H 'Authorization: Basic YWRtaW46Vm9sdGVycmExMjM=' \
   -H 'Accept: application/json, text/plain, */*' \
