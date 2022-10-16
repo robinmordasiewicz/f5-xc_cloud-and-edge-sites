7618909d244849234790d37de86104d899a86e87
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-10-16 00:38:44 -0400

    adding files

--- deploy-site.sh
+++ deploy-site.sh
@@ -33,24 +33,6 @@ timeout () {
 }
 
 
-read -p "Site Address: [801 5th Ave Seattle, WA 98104 United States] " address
-address="${address:=801 5th Ave Seattle, WA 98104 United States}"
-
-read -p "Site Latitude: [47.605199] " latitude
-latitude="${latitude:=47.605199}"
-
-read -p "Site Longitude: [-122.330996] " longitude
-longitude="${longitude:=-122.330996}"
-
-read -p "CE Node IP Addr or DNS name: [10.1.1.5] " cenodeaddress
-cenodeaddress="${cenodeaddress:=10.1.1.5}"
-
-read -p "CE Node Port: [65500] " cenodeport
-cenodeport="${cenodeport:=65500}"
-
-read -p "CE Node hostname: " nodename
-if [ ! "${nodename}" ]; then exit; fi
-
 read -s -p "CE node new password: " newpassword
 if [ ! "${newpassword}" ]; then exit; fi
 echo "*********************"
@@ -80,46 +62,36 @@ echo "# Create manifests"
 cd manifests/
 
 echo "# Create a k8s cluster object"
-jq -r ".metadata.name = \"${sitename}\" | .spec.cluster_wide_app_list.cluster_wide_apps[].argo_cd.local_domain.password.blindfold_secret_info.location = \"string:///${encryptedpassword}\" " k8s_cluster.json | sponge k8s_cluster.json
+jq -r ".spec.cluster_wide_app_list.cluster_wide_apps[].argo_cd.local_domain.password.blindfold_secret_info.location = \"string:///${encryptedpassword}\" " k8s_cluster.json | sponge k8s_cluster.json
 unset encryptedpassword
 
-jq -r ".metadata.name = \"${sitename}\" " k8s_cluster.json | sponge k8s_cluster.json
-git add k8s_cluster.json && git commit --quiet -m "creating deployment manifests"
 vesctl configuration apply k8s_cluster -i k8s_cluster.json
-
-jq -r ".spec.cluster_wide_app_list.cluster_wide_apps[].argo_cd.local_domain.password.blindfold_secret_info.location = \"<removed>\" " k8s_cluster.json | sponge k8s_cluster.json
-git add k8s_cluster.json && git commit --quiet -m "creating deployment manifests"
+git restore k8s_cluster.json
 
 echo "# Create an appstack site"
-jq -r ".metadata.name = \"${sitename}\" | .spec.k8s_cluster.name = \"${sitename}\" | .spec.master_nodes[] = \"${nodename}\" | .spec.address = \"${address}\" | .spec.coordinates.latitude = \"${latitude}\" | .spec.coordinates.longitude = \"${longitude}\" " appstack_site.json | sponge appstack_site.json
-git add appstack_site.json && git commit --quiet -m "creating deployment manifests"
 vesctl configuration apply voltstack_site -i appstack_site.json
 
 echo "# Create a site token for registration"
-jq -r ".metadata.name = \"${sitename}-token\" " token.json | sponge token.json
-git add token.json && git commit --quiet -m "creating deployment manifests"
 vesctl configuration apply token -i token.json
-timeout 15 "# Wait for token creation %s"
+timeout 10 "# Wait for token creation %s"
 token=`vesctl configuration get token ${sitename}-token --outfmt json -n system | jq -r ".system_metadata.uid"`
 
 echo "# Register the CE"
-jq -r ".token = \"${token}\" | .cluster_name = \"${sitename}\" | .hostname = \"${nodename}\" | .latitude = \"${latitude}\" | .longitude = \"${longitude}\" " ce-register.json | sponge ce-register.json
-git add ce-register.json && git commit --quiet -m "creating deployment manifests"
+jq -r ".token = \"${token}\" " ce-register.json | sponge ce-register.json
 curl -sS -k -v "https://${cenodeaddress}:${cenodeport}/api/ves.io.vpm/introspect/write/ves.io.vpm.config/update" \
   -H "Authorization: Basic ${basicauth}" \
   -H 'Content-Type: application/json' \
   -d @ce-register.json
 unset basicauth
+git restore ce-register.json
 
 timeout 20 "Wait for the registration to activate %s"
 
 echo "# Approve the registration"
-#registration=`vesctl configuration list registration -n system --outfmt json | jq '.items' | jq -r '.[0].name'`
-#registration=`vesctl request rpc registration.CustomAPI.ListRegistrationsByState -i pending.json --uri /public/namespaces/system/listregistrationsbystate --http-method POST | yq -o=json | jq '.items' | jq -r '.[0].name'`
 registration=`vesctl request rpc registration.CustomAPI.ListRegistrationsByState -i pending.json --uri /public/namespaces/system/listregistrationsbystate --http-method POST | yq -o=json | jq -r ".items[] | select(.getSpec.token == \"${token}\") | .name"`
-jq -r ".name = \"${registration}\" | .passport.cluster_name = \"${sitename}\" | .passport.latitude = \"${latitude}\" | .passport.longitude = \"${longitude}\" " approval_req.json | sponge approval_req.json
-git add approval_req.json && git commit --quiet -m "creating deployment manifests"
+jq -r ".name = \"${registration}\" " approval_req.json | sponge approval_req.json
 vesctl request rpc registration.CustomAPI.RegistrationApprove -i approval_req.json --uri /public/namespaces/system/registration/${registration}/approve --http-method POST
+git restore approval_req.json
 
 timeout 30 "Wait for the site to appear before provisioning %s"
 
@@ -145,7 +117,6 @@ if [[ "${STATE}" == "ONLINE" ]]; then
   echo "# Download a kubeconfig"
   expiration_timestamp=`date -u --date=tomorrow +%FT%T.%NZ`
   jq -r ".site = \"${sitename}\" | .expiration_timestamp = \"${expiration_timestamp}\" " download_kubeconfig.json | sponge download_kubeconfig.json
-  git add download_kubeconfig.json && git commit --quiet -m "creating deployment manifests"
   [ -d $HOME/.kube ] || mkdir $HOME/.kube
   curl -sS -v "https://${tenantname}.console.ves.volterra.io/api/web/namespaces/system/sites/${sitename}/global-kubeconfigs" \
     --key $HOME/vesprivate.key \
@@ -155,5 +126,6 @@ if [[ "${STATE}" == "ONLINE" ]]; then
     -d @download_kubeconfig.json \
     -o $HOME/.kube/ves_system_${sitename}_kubeconfig_global.yaml
 fi
+git restore download_kubeconfig.json
 
 kubectl --kubeconfig $HOME/.kube/ves_system_${sitename}_kubeconfig_global.yaml get pods -A
