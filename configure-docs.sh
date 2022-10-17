b18040395ea6818a4c5a2662b06ed79a77539426
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-10-17 04:11:23 -1000

    adding files

--- configure-docs.sh
+++ configure-docs.sh
@@ -3,5 +3,5 @@
 
 if [[ -e docs/rst_prolog.inc ]]; then rm docs/rst_prolog.inc; fi
 for keyfield in $(jq -r '.rst_prolog | keys[] as $k | "\($k)"' manifests/site.json); do
-  echo ".. |$keyfield| replace:: `jq -r ".rst_prolog.${keyfield}" manifests/site.json`" > docs/rst_prolog.inc
+  echo ".. |$keyfield| replace:: `jq -r ".rst_prolog.${keyfield}" manifests/site.json`" >> docs/rst_prolog.inc
 done
