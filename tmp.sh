195506c27b50668dfe4e1dff4825fd33cd2c9207
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-10-21 19:42:50 +0000

    adding files

--- tmp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-
-argocdpassword=Volterra123
-vesctl request secrets get-public-key > public-key.key
-vesctl request secrets get-policy-document --namespace shared --name ves-io-allow-volterra > secret-policy-ves-io-allow-volterra.crt
-echo -n ${argocdpassword} > password.key
-unset argocdpassword
-vesctl request secrets encrypt --policy-document secret-policy-ves-io-allow-volterra.crt --public-key public-key.key password.key
