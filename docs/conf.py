70d7eec7ea0dcd796c5d980a43ef061bfa46015d
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-10-24 08:43:27 -0400

    adding files

--- docs/conf.py
+++ docs/conf.py
@@ -154,7 +154,7 @@ source_suffix = [".rst"]
 master_doc = "index"
 
 # General information about the project.
-project = "Cloud and Edge Sites"
+project = "Workspaces"
 author = "Robin Mordasiewicz"
 
 # The version info for the project you're documenting, acts as replacement for
--- docs/index.rst
+++ docs/index.rst
@@ -1,6 +1,13 @@
+:sd_hide_title:
+
 Cloud and Edge Sites
 ====================
 
 .. toctree::
+   :hidden:
 
    cloud-and-edge-sites.rst
+
+.. topic:: Workspaces
+
+   Make a common workspace page
